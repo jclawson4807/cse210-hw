77f82a069381149e255601697e462810dec52266
jclawson4807 <jclawsonx@icloud.com>
2025-01-06 19:13:56 -0600

    Completed w1 e2 s2 and s3

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,37 +5,69 @@ class Program
 {
     static void Main(string[] args)
     {
+        /*
+        Ask the user for their grade percentage, then write a series of if, else if, else statements to print out
+        the appropriate letter grade. (At this point, you'll have a separate print statement for each grade letter 
+        in the appropriate block.)
+
+        Assume that you must have at least a 70 to pass the class. After determining the letter grade and printing it 
+        out. Add a separate if statement to determine if the user passed the course, and if so display a message to 
+        congratulate them. If not, display a different message to encourage them for next time.
+
+        Change your code from the first part, so that instead of printing the letter grade in the body of each if, else 
+        if, or else block, instead create a new variable called letter and then in each block, set this variable to the 
+        appropriate value. Finally, after the whole series of if, else if, else statements, have a single print statement 
+        that prints the letter grade once.
+        */
+        
         Console.Write("What is your grade percentage? ");
         string gradePercentageString = Console.ReadLine();
 
         float gradePecentageFloat = float.Parse(gradePercentageString);
 
-        string letterGrade = "E";
+        string letter = "E";
+        bool didPass = false;
 
         if (gradePecentageFloat >= 90)
         {
-            letterGrade = "A";
-            Console.WriteLine($"Your grade is an {letterGrade}");
+            letter = "A";
+            didPass = true;
         }
         else if (gradePecentageFloat >= 80)
         {
-            letterGrade = "B";
-            Console.WriteLine($"Your grade is a {letterGrade}");
+            letter = "B";
+            didPass = true;
         }
         else if (gradePecentageFloat >= 70)
         {
-            letterGrade = "C";
-            Console.WriteLine($"Your grade is a {letterGrade}");
+            letter = "C";
+            didPass = true;
         }
         else if (gradePecentageFloat >= 60)
         {
-            letterGrade = "D";
-            Console.WriteLine($"Your grade is a {letterGrade}");
+            letter = "D";
         }
         else
         {
-            letterGrade = "F";
-            Console.WriteLine($"Your grade is a {letterGrade}");    
+            letter = "F";   
+        }
+
+        if (letter == "A")
+        {
+            Console.WriteLine($"Your grade is an {letter}");
+        }
+        else
+        {
+            Console.WriteLine($"Your grade is a {letter}");
+        }
+
+        if (didPass)
+        {
+            Console.WriteLine("Contraulations!  You passed the class!");    
+        }
+        else
+        {
+            Console.WriteLine("You did not pass the class.  Try harder next time!");    
         }
     }
 }
