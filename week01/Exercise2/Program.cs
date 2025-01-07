3bfcfc924401319e394b26adeaecded57e01c1ed
jclawson4807 <jclawsonx@icloud.com>
2025-01-06 19:06:52 -0600

    Completed W1 E2 S1

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,41 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string gradePercentageString = Console.ReadLine();
+
+        float gradePecentageFloat = float.Parse(gradePercentageString);
+
+        string letterGrade = "E";
+
+        if (gradePecentageFloat >= 90)
+        {
+            letterGrade = "A";
+            Console.WriteLine($"Your grade is an {letterGrade}");
+        }
+        else if (gradePecentageFloat >= 80)
+        {
+            letterGrade = "B";
+            Console.WriteLine($"Your grade is a {letterGrade}");
+        }
+        else if (gradePecentageFloat >= 70)
+        {
+            letterGrade = "C";
+            Console.WriteLine($"Your grade is a {letterGrade}");
+        }
+        else if (gradePecentageFloat >= 60)
+        {
+            letterGrade = "D";
+            Console.WriteLine($"Your grade is a {letterGrade}");
+        }
+        else
+        {
+            letterGrade = "F";
+            Console.WriteLine($"Your grade is a {letterGrade}");    
+        }
     }
 }
