5f16784a28b823186b802fea5013c2b11898d3f1
jclawson4807 <jclawsonx@icloud.com>
2025-01-07 20:12:20 -0600

    added PromptUserName Function

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -7,7 +7,7 @@ class Program
     For this assignment, write a C# program that has several simple functions:
 
     
-    PromptUserName - Asks for and returns the user's name (as a string)
+    
     PromptUserNumber - Asks for and returns the user's favorite number (as an integer)
     SquareNumber - Accepts an integer as a parameter and returns that number squared (as an integer)
     DisplayResult - Accepts the user's name and the squared number and displays them.
@@ -20,7 +20,14 @@ class Program
         Console.WriteLine("Welcome to the Program!");
     }
 
-    
+    // PromptUserName - Asks for and returns the user's name (as a string)
+    static string PromptUserName()
+    {
+        Console.WriteLine("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
 
     static void Main(string[] args)
     {
