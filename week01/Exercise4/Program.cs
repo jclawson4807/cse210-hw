fcae562739ae0dc205b7a39000a7a3b84747da26
jclawson4807 <jclawsonx@icloud.com>
2025-01-07 20:18:12 -0600

    added SquareNumber function

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -9,7 +9,7 @@ class Program
     
     
     
-    SquareNumber - Accepts an integer as a parameter and returns that number squared (as an integer)
+    
     DisplayResult - Accepts the user's name and the squared number and displays them.
     Your Main function should then call each of these functions saving the return values and passing data to them as necessary.
     */
@@ -45,6 +45,13 @@ class Program
             Console.WriteLine("You must enter an integer value.");
             throw;
         }
+    }
+
+    // SquareNumber - Accepts an integer as a parameter and returns that number squared (as an integer)
+    static int SquareNumber(int valueToSquare)
+    {
+        int squaredValue = valueToSquare * valueToSquare;
+        return squaredValue;
     }
 
     static void Main(string[] args)
