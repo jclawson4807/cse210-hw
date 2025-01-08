f953a69decb34ffa62fc99b6f8422a96f566b3c3
jclawson4807 <jclawsonx@icloud.com>
2025-01-07 20:10:40 -0600

    Begin work on the Functions assigmnent for week 1

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -2,6 +2,26 @@ using System;
 
 class Program
 {
+
+    /*
+    For this assignment, write a C# program that has several simple functions:
+
+    
+    PromptUserName - Asks for and returns the user's name (as a string)
+    PromptUserNumber - Asks for and returns the user's favorite number (as an integer)
+    SquareNumber - Accepts an integer as a parameter and returns that number squared (as an integer)
+    DisplayResult - Accepts the user's name and the squared number and displays them.
+    Your Main function should then call each of these functions saving the return values and passing data to them as necessary.
+    */
+
+    // DisplayWelcome - Displays the message, "Welcome to the Program!"
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    
+
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise4 Project.");
