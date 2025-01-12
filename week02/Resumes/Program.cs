fefa74ecd63fc19173f1b04644076123bba8ca8a
jclawson4807 <jclawsonx@icloud.com>
2025-01-12 17:26:27 -0600

    create job instance, set values, display

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "BYU";
+        job1._startYear = 1991;
+        job1._endYear = 1992;
+
+        job1.Display();
+
     }
 }
