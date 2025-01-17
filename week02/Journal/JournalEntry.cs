deec80bdf528043da028f9f9453c19537da48da7
jclawson4807 <jclawsonx@icloud.com>
2025-01-16 21:59:57 -0600

    beginning code work on Journal project

---
+++ week02/Journal/JournalEntry.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class JournalEntry
+{
+    public string _prompt;
+    public string _journalEntryText;
+    public DateTime _journalEntryTimeStamp;
+
+    public void Display()
+    {
+        
+    }
+}
