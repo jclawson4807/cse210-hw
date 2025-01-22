0fd119b66a91c79eb6f8c07fdc5f407ee6ef5739
jclawson4807 <jclawsonx@icloud.com>
2025-01-21 23:19:26 -0600

    Core program works

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -2,18 +2,36 @@ using System;
 
 class Program
 {
+    static Scripture scripture = new Scripture("3 Nephi 5:13", "Behold, I am a disciple of Jesus Christ, the Son of God. I have been called of him to declare his word among his people, that they might have everlasting life.");
+
     static void Main(string[] args)
     {
-        Scripture scripture = new Scripture("3 Nephi 5:13", "Behold, I am a disciple of Jesus Christ, the Son of God. I have been called of him to declare his word among his people, that they might have everlasting life.");
+        DisplayScriptureWithMenu();    
+    }
 
-        scripture.DisplayScripture();
+    public static void DisplayScriptureWithMenu()
+    {
+        scripture.DisplayScripture();  
 
-        scripture.HideWords(false);
+        Console.WriteLine();
+        Console.WriteLine();
 
-        scripture.DisplayScripture();
+        Console.WriteLine("Press enter to continue or type 'quit' to finish:");
 
-        scripture.HideWords(false);
+        string readLine = Console.ReadLine();
 
-        scripture.DisplayScripture();
+        if (readLine.Length == 0)
+        {
+            scripture.HideWords(false);
+
+            DisplayScriptureWithMenu();    
+        }
+        else
+        {
+            if (readLine.ToLower().Trim() == "quit")
+            {
+                Environment.Exit(0);
+            }
+        }
     }
 }
