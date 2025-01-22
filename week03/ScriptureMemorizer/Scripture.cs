a7bd93c3afc0bb8232792518bf4803f08d7cb73f
jclawson4807 <jclawsonx@icloud.com>
2025-01-21 18:57:37 -0600

    wrote code to break scripture into words, spaces, and punctuation

--- week03/ScriptureMemorizer/Scripture.cs
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -10,7 +10,24 @@ class Scripture
 
     private void PopulateWordListForScriptureText(string scriptureText)
     {
+        _wordsList.Clear();
+        
+        string[] tokens = scriptureText.Split(new char[] { ' ', ',', '?', '.', '!' });
+        
+        foreach (string token in tokens) {
+            bool isWord = false;
+            int tokenLength = token.Length;
+            string testToken = token.ToLower();
 
+            if (tokenLength > 1 || (testToken == "a" || testToken == "i" || testToken == "a"))
+            {
+                isWord = true;
+            }
+
+            Word newWord = new Word(token,isWord);
+
+            _wordsList.Add(newWord);
+        }
     }
 
     public Scripture(ScriptureReference scriptureReference, string scriptureText)
