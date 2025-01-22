758ad92f3a36ea325d677b3946f0727d8b47517b
jclawson4807 <jclawsonx@icloud.com>
2025-01-21 19:25:44 -0600

    test initial scripture parser

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        Scripture scripture = new Scripture("3 Nephi 5:13", "Behold, I am a disciple of Jesus Christ, the Son of God. I have been called of him to declare his word among his people, that they might have everlasting life.");
+
+        scripture.DisplayScripture();
+
+
     }
 }
