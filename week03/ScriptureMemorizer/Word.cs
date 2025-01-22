6d389a48e3587e5bde54b8e4b9e11f3e96e0192d
jclawson4807 <jclawsonx@icloud.com>
2025-01-21 18:18:22 -0600

    Finished Word class

--- week03/ScriptureMemorizer/Word.cs
+++ week03/ScriptureMemorizer/Word.cs
@@ -6,4 +6,43 @@ class Word
     private string _hiddenWord = "";
     private int _wordLength;
     private bool _isHidden = false;
+
+    public Word(string word)
+    {
+        _word = word;
+        _wordLength = word.Length;
+        CreateHiddenWord();
+    }
+
+    private void CreateHiddenWord()
+    {
+        _hiddenWord = "";
+
+        for (int i = 0; i < _wordLength; i++)
+        {
+            _hiddenWord = _hiddenWord + "_";
+        }
+    }
+
+    public string GetWord()
+    {
+        if (_isHidden)
+        {
+            return _hiddenWord; 
+        }
+        else
+        {
+            return _word;
+        }
+    }
+
+    public void SetIsHidden(bool isHidden)
+    {
+        _isHidden = isHidden;
+    }
+
+    public bool GetIsHidden()
+    {
+        return _isHidden;
+    }
 }
