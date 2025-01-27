939657f87458c9c40eb087f1fc5f4710b5fba11f
jclawson4807 <jclawsonx@icloud.com>
2025-01-26 21:21:22 -0600

    add code to get total order cost

--- week04/OnlineOrdering/Order.cs
+++ week04/OnlineOrdering/Order.cs
@@ -5,13 +5,33 @@ public class Order
     private Customer _customer;
     private List<Product> _productList = new List<Product>();
 
-    public double GetTotalOrderCost()
+    private double GetTotalOrderProductCost()
     {
-        double totalOrderCost = 0.0;
+        double totalProductCost = 0.0;
 
         foreach (Product product in _productList)
         {
-            totalOrderCost += product.GeTotalProductCost();
+            totalProductCost += product.GeTotalProductCost();
+        }
+
+        return totalProductCost;
+    }
+    
+    public double GetTotalOrderCost()
+    {
+        double totalOrderCost = GetTotalOrderProductCost();
+
+        // add shipping cost to the total order cost
+
+        if (_customer.DoesCustomerLiveInUnitedStates())
+        {
+            // if customer lives in the US, the shipping cost is $5
+            totalOrderCost += 5.0;
+        }
+        else
+        {
+            // if customer lives outside of the US, the shipping cost is $35
+            totalOrderCost += 35.0;
         }
 
         return totalOrderCost;
@@ -19,7 +39,14 @@ public class Order
 
     public string GetPackingLabel()
     {
-        return "";
+        string packingLabel = "";
+
+        foreach (Product product in _productList)
+        {
+            
+        }
+
+        return packingLabel;
     }
 
     public string GetShippingLabel()
