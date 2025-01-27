57a023c51ad24e1936d215a14e0d1a2325813d94
jclawson4807 <jclawsonx@icloud.com>
2025-01-26 21:03:05 -0600

    finished base Address class

--- week04/OnlineOrdering/Address.cs
+++ week04/OnlineOrdering/Address.cs
@@ -6,4 +6,29 @@ public class Address
     private string _city;
     private string _stateOrProvince;
     private string _country;
+
+    public Address(string streetAddress, string city, string stateOrProvince, string country)
+    {
+        _streetAddress = streetAddress;
+        _city = city;
+        _stateOrProvince = stateOrProvince;
+        _country = country;
+    }
+
+    public bool IsAddressInUnitedStates()
+    {
+        if (_country.ToLower() == "us" || _country.ToLower() == "usa")
+        {
+            return true;
+        } 
+        else
+        {
+            return false;
+        }
+    }
+
+    public string GetFormattedAddress()
+    {
+        return $"{_streetAddress}\n{_city}, {_stateOrProvince} {_country}";
+    }
 }
