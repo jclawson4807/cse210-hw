3a23d919b4ddaeedc55bffd9dd30dd0df86ec289
jclawson4807 <jclawsonx@icloud.com>
2025-01-26 21:54:40 -0600

    added third order
    same products as second order, but sent to US address to show difference in price

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -14,13 +14,21 @@ class Program
 
         _orders.Add(order1);
 
-        Order order2 = new Order("Mauricio Velez-Koolwont ", "16, Jules Koenig Street", "Port-Louis", "Port Louis", "Mauritius");
+        Order order2 = new Order("Mauricio Velez-Koolwont", "16, Jules Koenig Street", "Port-Louis", "Port Louis", "Mauritius");
 
         order2.AddProduct("GOOD GOOD No Added Sugar Fruit Jam Variety Pack of 4", "B0C61YPX9B", 27.99, 2);
         order2.AddProduct("Rawlings Official NCAA Air It Out Gametime Football - BYU", "B079K8LXWR", 15.99, 6);
         order2.AddProduct("LLQ 8Pcs Mini Lantern with LED Tealight, Ramadan Lanterns", "B09VKZQ5Y9", 26.99, 2);
 
         _orders.Add(order2);
+
+        Order order3 = new Order("Roger Matthews", "1622 W Forest Lane", "Cedar Hills", "UT", "USA");
+
+        order3.AddProduct("GOOD GOOD No Added Sugar Fruit Jam Variety Pack of 4", "B0C61YPX9B", 27.99, 2);
+        order3.AddProduct("Rawlings Official NCAA Air It Out Gametime Football - BYU", "B079K8LXWR", 15.99, 6);
+        order3.AddProduct("LLQ 8Pcs Mini Lantern with LED Tealight, Ramadan Lanterns", "B09VKZQ5Y9", 26.99, 2);
+
+        _orders.Add(order3);
     }
 
     static void Main(string[] args)
