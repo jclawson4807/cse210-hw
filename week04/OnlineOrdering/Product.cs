02a01415841efd330e309274cee299aff829ef47
jclawson4807 <jclawsonx@icloud.com>
2025-01-26 20:56:02 -0600

    base product class completed

---
+++ week04/OnlineOrdering/Product.cs
@@ -0,0 +1,23 @@
+using System;
+
+public class Product
+{
+    private string _productName;
+    private string _productId;
+
+    private double _price;
+    private int _quantity;
+
+    public Product(string productName, string productId, double price, int quantity)
+    {
+        _productName = productName;
+        _productId = productId;
+        _price = price;
+        _quantity = quantity;
+    }
+
+    public double GeTotalProductCost()
+    {
+        return _price * _quantity;
+    }
+}
