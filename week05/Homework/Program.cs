2024c4ad6d7d03c4a572cf550a44ea096be828fb
jclawson4807 <jclawsonx@icloud.com>
2025-02-01 17:01:38 -0600

    finished Homework assignment

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -8,10 +8,16 @@ class Program
         Assignment assignment1 = new Assignment("Samuel Bennett", "Multiplication");
 
         Console.WriteLine(assignment1.GetSummary());
-
+        Console.WriteLine("");
         // test step 4
         MathAssignment mathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
         Console.WriteLine(mathAssignment.GetSummary());
         Console.WriteLine(mathAssignment.GetHomeworkList());
+        Console.WriteLine("");
+
+        // test step 5
+        WritingAssignment writingAssignment = new WritingAssignment("Mary Watters", "European History", "The Causes of World War II");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
