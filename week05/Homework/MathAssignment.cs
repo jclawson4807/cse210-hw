dcc40e790f45721a5d7361c2e5cad42089cdf664
jclawson4807 <jclawsonx@icloud.com>
2025-02-01 16:58:04 -0600

    fixed : in MathProblems
    I am too tired to write code.  I need to sleep.

--- week05/Homework/MathAssignment.cs
+++ week05/Homework/MathAssignment.cs
@@ -3,7 +3,7 @@ using System;
 public class MathAssignment : Assignment
 {
     private string _textbookSection;
-    private string _problems:
+    private string _problems;
 
     public MathAssignment(string studentName, string topic, string textbookSection, string problems) : base(studentName, topic)
     {
--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,11 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        // test step 3.5 - 
+        // test step 3
         Assignment assignment1 = new Assignment("Samuel Bennett", "Multiplication");
 
         Console.WriteLine(assignment1.GetSummary());
 
+        // test step 4
         MathAssignment mathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
     }
 }
