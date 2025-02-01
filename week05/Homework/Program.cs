e6ae750b79297858364941d5f2aa7d9dc2e8d0ca
jclawson4807 <jclawsonx@icloud.com>
2025-02-01 16:55:36 -0600

    testing MathAssignment class

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,9 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        // test step 1 - 
+        // test step 3.5 - 
         Assignment assignment1 = new Assignment("Samuel Bennett", "Multiplication");
 
         Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment mathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
     }
 }
