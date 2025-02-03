c13fb9369ccfca22dfea32f5ac0ac5390fc003e3
jclawson4807 <jclawsonx@icloud.com>
2025-02-03 15:11:07 -0600

    added DisplayArrowSpinnerForCountdownTimer

--- week05/Mindfulness/Animation.cs
+++ week05/Mindfulness/Animation.cs
@@ -38,4 +38,42 @@ public class Animation
             }
         }
     }
+
+    public void DisplayArrowSpinnerForCountdownTimer(int totalNumberOfSeconds, bool progressiveSpinner = false)
+    {
+        List<string> spinnerCharacters = new List<string>();
+
+        spinnerCharacters.Add("<");
+        spinnerCharacters.Add("^");
+        spinnerCharacters.Add(">");
+        spinnerCharacters.Add("v");
+
+        int i = 0;
+        int c = spinnerCharacters.Count;
+
+        DateTime startTime = DateTime.Now;
+        DateTime endTime = startTime.AddSeconds(totalNumberOfSeconds);
+
+        while (DateTime.Now < endTime)
+        {
+            Console.Write(spinnerCharacters[i]);
+            Thread.Sleep(1000);
+
+            if (progressiveSpinner)
+            {
+                Console.Write("\b  \b");
+            }
+            else
+            {
+                Console.Write("\b \b");
+            }
+
+            i++;
+
+            if (i >= c)
+            {
+                i = 0;
+            }
+        }
+    }
 }
--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -7,7 +7,8 @@ class Program
         Console.WriteLine("Hello World! This is the Mindfulness Project.");
 
         Animation animation = new Animation();
-        animation.DisplayCountdownTimer(totalNumberOfSeconds: 11);
-        animation.DisplayProgressiveCountdownTimer(totalNumberOfSeconds: 30);
+        // animation.DisplayCountdownTimer(totalNumberOfSeconds: 11);
+        // animation.DisplayProgressiveCountdownTimer(totalNumberOfSeconds: 30);
+        animation.DisplayArrowSpinnerForCountdownTimer(totalNumberOfSeconds: 30, progressiveSpinner: true);
     }
 }
