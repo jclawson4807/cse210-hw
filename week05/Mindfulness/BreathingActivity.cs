bebdce5773700bad689877b8c6250825212e24c7
jclawson4807 <jclawsonx@icloud.com>
2025-02-04 21:16:01 -0600

    fixed activity descriptions

--- week05/Mindfulness/BreathingActivity.cs
+++ week05/Mindfulness/BreathingActivity.cs
@@ -9,7 +9,7 @@ public class BreathingActivity: Activity
     {
         SetActivityName("Breathing Activity");
         SetWelcomeMessage("Welcome to the Breathing Activity.");
-        SetActivityDescriptionMessage("This activity will help you relax by walking you through breathing in and out slowly.  Clear your mind and focus on your breathing.");
+        SetActivityDescriptionMessage("This activity will help you relax by walking you through breathing in and out slowly. Clear your mind and focus on your breathing.");
         SetHowLongMessage("How long, in sections, would you like for your session?");
         SetAfterActivityMessage("You have completed another ", " seconds of the Breathing Activity.  Good job!");
     }
--- week05/Mindfulness/ListingActivity.cs
+++ week05/Mindfulness/ListingActivity.cs
@@ -8,6 +8,6 @@ public class ListingActivity: Activity
         SetWelcomeMessage("Welcome to the Listing Activity.");
         SetActivityDescriptionMessage("This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.");
         SetHowLongMessage("How long, in sections, would you like for your session?");
-        SetAfterActivityMessage("You have completed another ", "seconds of the Listing Activity.  Good job!");
+        SetAfterActivityMessage("You have completed another ", " seconds of the Listing Activity.  Good job!");
     }
 }
