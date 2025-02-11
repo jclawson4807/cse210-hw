71fec22ad1bd4d0d8e68430eb07dcf876cad2074
jclawson4807 <jclawsonx@icloud.com>
2025-02-10 19:45:17 -0600

    starting Shape.cs

---
+++ week06/Shapes/Shape.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Shape
+{
+    private string _color;
+
+    public string GetColor()
+    {
+        return _color;
+    }
+
+    public void SetColor(string color)
+    {
+        _color = color;
+    }
+}
