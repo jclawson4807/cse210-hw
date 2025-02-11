0d3d47c62c804f07d87fe110f3a8ed80b355084e
jclawson4807 <jclawsonx@icloud.com>
2025-02-10 22:59:32 -0600

    Implemented basic eternal goal

---
+++ week06/EternalQuest/EternalGoal.cs
@@ -0,0 +1,40 @@
+using System;
+
+public class EternalGoal : Goal
+{
+    public EternalGoal() : base ()
+    {
+
+    }
+    
+    public EternalGoal(string title, string description, int points) : base(title: title, description: description, points: points)
+    {
+        Console.WriteLine("Created Eternal Goal");
+    }
+
+    public override EternalGoal DisplayCreateGoalMenu()
+    {
+        Console.Clear();
+        Console.Write("What is the name of your goal? ");
+        string title = Console.ReadLine();
+
+        Console.Write("\nWhat is a short description of your goal? ");
+        string description = Console.ReadLine();
+
+        Console.Write("\nWhat is the amount of points associated with this goal? ");
+        string numberOfPoints = Console.ReadLine();
+
+        int points = int.Parse(numberOfPoints);
+
+        EternalGoal eternalGoal = new EternalGoal(title: title, description: description, points: points);
+
+        return eternalGoal;
+    }
+
+    public override string GetGoalDisplayString()
+    {
+        string goalDisplayString = $"[ ] {goalDisplayString}] {GetTitle()} ({GetDescription()})"; 
+
+        return goalDisplayString;
+    }
+}
