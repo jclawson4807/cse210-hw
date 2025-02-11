a9a228923ab97ed445348226c9fa5bcaff3706b7
jclawson4807 <jclawsonx@icloud.com>
2025-02-10 23:19:41 -0600

    added todo list

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -23,6 +23,16 @@ class Program
         checklistGoal = checklistGoal.DisplayCreateGoalMenu();
 
         Console.WriteLine(checklistGoal.GetGoalDisplayString());
+        /*
+        @TODO - 
+        1) create a list to store the goals
+        2) create the primary menu
+        3) adding code to store current points
+        4) write code in base and derived classes associated with marking a goal done and getting back points
+        5) add save and load functionality
+            this will require a version of the get goal string that returns a storable version rather than the display version
+        6) add custom functionality - add a goal type where the score increases based on how many times it is completed in a given day or how much you exceeded a base amount .... such as drinking water, walking, etc. perhaps a ranking system, something that prevents a goal from being accomplished too many times in a given day, a goal editor
+        */
 
     }
 }
