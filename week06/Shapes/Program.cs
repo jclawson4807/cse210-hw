928c0fb343ce577861e229d70002e54b5d3bf493
jclawson4807 <jclawsonx@icloud.com>
2025-02-10 20:02:19 -0600

    finished program

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,8 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Square square = new Square(color: "blue", side: 2.7);
+        Square square = new Square(color: "Blue", side: 2.7);
+        Rectangle rectangle = new Rectangle(color: "Green", length: 2.7, width: 7.2);
+        Circle circle = new Circle(color: "Green", radius: 2.7);
 
-        Console.WriteLine($"{square.GetColor()} {square.GetArea()}");
+        Console.WriteLine($"Square {square.GetColor()} {square.GetArea()}");
+        Console.WriteLine($"Rectangle {rectangle.GetColor()} {rectangle.GetArea()}");
+        Console.WriteLine($"Circle {circle.GetColor()} {circle.GetArea()}");
     }
 }
