d3b16f1c97131b489371e9f99a1a1c9ab9d31ee1
jclawson4807 <jclawsonx@icloud.com>
2025-02-10 22:32:54 -0600

    added getters and setters

--- week06/EternalQuest/Goal.cs
+++ week06/EternalQuest/Goal.cs
@@ -2,10 +2,16 @@ using System;
 
 public abstract class Goal
 {
+    private string _goalType = "Goal";
     private string _title;
     private string _description;
     private int _points;
     private bool _isComplete = false;
+
+    public Goal()
+    {
+
+    }
 
     public Goal(string title, string description, int points)
     {
@@ -14,10 +20,52 @@ public abstract class Goal
         _points = points;
     }
 
+    public string GetGoalType()
+    {
+        return _goalType;
+    }
+
+    public void SetTitle(string title)
+    {
+        _title = title;
+    }
+
+    public string GetTitle()
+    {
+        return _title;
+    }
+
+    public void SetDescription(string description)
+    {
+        _description = description;
+    }
+
+    public string GetDescription()
+    {
+        return _description;
+    }
+
+    public void SetPoints(int points)
+    {
+        _points = points;
+    }
+
+    public int GetPoints()
+    {
+        return _points;
+    }
+
     public void SetIsGoalComplete(bool isGoalComplete)
     {
         _isComplete = isGoalComplete;
     }
+
+    public bool GetIsGoalComplete()
+    {
+        return _isComplete;
+    }
+
+    public abstract Goal DisplayCreateGoalMenu();
 
     public abstract string GetGoalDisplayString();
 }
