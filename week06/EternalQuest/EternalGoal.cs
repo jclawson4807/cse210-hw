91978896235f82fd2e3869610aa0960d73f41f95
jclawson4807 <jclawsonx@icloud.com>
2025-02-12 19:32:48 -0600

    added DisplayGoalEditor for EternalGoal

--- week06/EternalQuest/EternalGoal.cs
+++ week06/EternalQuest/EternalGoal.cs
@@ -4,11 +4,12 @@ public class EternalGoal : Goal
 {
     public EternalGoal() : base ()
     {
-
+        SetGoalType("EternalGoal");
     }
     
     public EternalGoal(string title, string description, int points) : base(title: title, description: description, points: points)
     {
+        SetGoalType("EternalGoal");
         Console.WriteLine("Created Eternal Goal");
     }
 
@@ -46,5 +47,53 @@ public class EternalGoal : Goal
         string goalDisplayString = $"[ ] {GetTitle()} ({GetDescription()})"; 
 
         return goalDisplayString;
+    }
+
+    public override EternalGoal DisplayGoalEditor()
+    {
+        EternalGoal newEternalGoal = new EternalGoal();
+        
+        Console.Write($"\nPlease enter the new Goal name, or hit enter to use the existing value: ({GetTitle()}): ");
+
+        string title = Console.ReadLine();
+
+        if (title.Trim().Length() > 0)
+        {
+            newEternalGoal.SetTitle(title.Trim());
+        }
+        else
+        {
+            newEternalGoal.SetTitle(GetTitle());     
+        }
+
+        Console.Write($"\nPlease enter the new description, or hit enter to use the existing value: ({GetDescription()}): ");
+
+        string description = Console.ReadLine();
+
+        if (description.Trim().Length() > 0)
+        {
+            newEternalGoal.SetDescription(description.Trim());
+        }
+        else
+        {
+            newEternalGoal.SetDescription(GetDescription());
+        }
+
+        Console.Write($"\nPlease enter the new point value for this goal, or hit enter to use the existing value: ({GetPoints()}): ");
+
+        string pointsString = Console.ReadLine();
+
+        if (pointsString.Length() > 0)
+        {
+            int points = ExtractIntFromString(pointsString);
+
+            newEternalGoal.SetPoints(points);
+        }
+        else
+        {
+            newEternalGoal.SetPoints(GetPoints());    
+        }
+
+        return newEternalGoal;
     }
 }
