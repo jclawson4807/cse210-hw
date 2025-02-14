f11b3ea51689b3089ea46c963b0863f8ab8981f4
jclawson4807 <jclawsonx@icloud.com>
2025-02-13 19:57:57 -0600

    implemented override methods in Swimming class

--- week07/ExerciseTracking/Swimming.cs
+++ week07/ExerciseTracking/Swimming.cs
@@ -3,19 +3,20 @@ using System;
 public class Swimming : Activity
 {
     private int _numberOfLaps;
+    private int _lapLengthInMeters = 50;
 
-    public override double GetDistance();
+    public override double GetDistance()
     {
-        
-    };
+        return _numberOfLaps * 50 / 1000 * 0.62;   
+    }
 
-    public override double GetSpeed();
+    public override double GetSpeed()
     {
-        
-    };
+        return (GetDistance() / GetActivityDuration()) * 60;
+    }
 
     public override double GetPace()
     {
-
-    };
+        return GetActivityDuration() / GetDistance();
+    }
 }
