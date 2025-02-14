b3104c2999d4a9817ffd13e40b62dc636a944884
jclawson4807 <jclawsonx@icloud.com>
2025-02-13 20:22:05 -0600

    Added constructor to Activity

--- week07/ExerciseTracking/Activity.cs
+++ week07/ExerciseTracking/Activity.cs
@@ -2,14 +2,31 @@ using System;
 
 public abstract class Activity
 {
+    private string _activityName;
     private DateTime _activityDate;
     private int _activityDurationInMinutes;
+
+    public Activity(DateTime activityDate, int activityDurationInMinutes)
+    {
+        _activityDate = activityDate;
+        _activityDurationInMinutes = activityDurationInMinutes;
+    }
+
+    public void SetActivityName(string activityName)
+    {
+        _activityName = activityName;
+    }
 
     public abstract double GetDistance();
 
     public abstract double GetSpeed();
 
     public abstract double GetPace();
+
+    public string GetActivityName()
+    {
+        return _activityName;
+    }
 
     public DateTime GetActivityDate()
     {
@@ -23,7 +40,7 @@ public abstract class Activity
 
     public string GetSummary()
     {
-
+        return $"{GetActivityDate().ToString("dd MMM yyyy")} {GetActivityName()} ({GetActivityDuration()} min)- Distance {GetDistance()} miles, Speed {GetSpeed()} mph, Pace: {GetPace()} min per mile";
     }
 
 }
