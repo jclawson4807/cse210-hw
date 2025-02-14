09c55d93d0e2c37b4570415000a28389acdd9b59
jclawson4807 <jclawsonx@icloud.com>
2025-02-13 20:37:37 -0600

    testing base program

--- week07/ExerciseTracking/Cycling.cs
+++ week07/ExerciseTracking/Cycling.cs
@@ -5,6 +5,9 @@ public class Cycling : Activity
     private double _speedInMph;
     private double _distanceInMiles;
 
+    // NOTE - from assignment - For each activity, they do not want to store this information, but they would like to be able to get following information (by calculation if it is not stored directly):
+    // does this mean we are not supposed to store the distance?
+
     public Cycling(DateTime activityDate, int activityDurationInMinutes, double distanceInMiles, double speedInMph) : base(activityDate: activityDate, activityDurationInMinutes: activityDurationInMinutes)
     {
         _distanceInMiles = distanceInMiles;
--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -2,8 +2,27 @@ using System;
 
 class Program
 {
+    private static List<Activity> _activities = new List<Activity>();
+
+    public static void PopulateActivities()
+    {
+        Cycling cycling = new Cycling(DateTime.Now, 120, 25, 12.5);
+        _activities.Add(cycling);
+
+        Running running = new Running(DateTime.Now, 60, 6);
+        _activities.Add(running);
+
+        Swimming swimming = new Swimming(DateTime.Now, 30, 15);
+        _activities.Add(swimming);
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        PopulateActivities();
+
+        foreach (Activity activity in _activities)
+        {
+            Console.WriteLine(activity.GetSummary());     
+        }
     }
 }
